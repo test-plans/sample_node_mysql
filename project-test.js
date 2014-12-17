de88231817ab4ca6ab566fec021347f2c0da118d
buildsample <buildsampletest@gmail.com>
2014-12-17 11:41:49 +0530

    Update project-test.js

--- project-test.js
+++ project-test.js
@@ -16,10 +16,10 @@ describe('MySql Database', function () {
   });
   it('should retrieve that name', function () {
     db('things')
-      .where({ name: 'Johnson' })
+      .where({ name: 'Johnnson' })
       .select('name')
       .then(function (name) {
-        expect(name[0].name).to.equal('Johson');
+        expect(name[0].name).to.equal('Johnson');
       });
   });
 });
